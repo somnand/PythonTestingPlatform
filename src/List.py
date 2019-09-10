45aff68169839229f0c35e04b59614063ae02595
somnand <somsuryananda@gmail.com>
2019-09-10 16:47:30 +0530

    User Entry needs more work. Added collection items.

---
+++ src/List.py
@@ -0,0 +1,8 @@
+#List programs
+list = [0,1,2,3,4]
+print("Original list "+list.__str__())
+print("First : "+list[1].__str__())
+print("Last : "+list[-1].__str__())
+print(list[:3])
+list.append(5)
+print(list)
---
+++ src/UserEntry.py
@@ -0,0 +1,4 @@
+#Program to accept user entry and recat accordingly
+N = input("Enter location to print (0-4) ")
+
+
